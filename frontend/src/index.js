06ab2ed7eb54b7134cc7371f7b72ee3de71fc084
vishal086 <vishalkumar359877@gmail.com>
2024-09-13 14:53:31 +0530

    Backend Url Added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,8 @@ import { persistStore } from 'redux-persist';
  
 let persistor = persistStore(store);  
 
-export const BASE_URL="http://localhost:8080"
+// export const BASE_URL="http://localhost:8080"
+export const BASE_URL = "https://chat-application-backend-bbs2.onrender.com"
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
